747c9d50fdc68531f62f5c8c8344bf01e15ca8b5
Pablo Jurado <pjurado@overnitecbt.com>
2017-07-06 16:02:40 -0500

    move all state to a single obj

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-let board = null
-const rowIndx = 10
-const colIndx = 10
-const minesNum = 20
-let isGameOver = false
+let AppState = {
+  board: null,
+  rowIndx: 10,
+  colIndx: 10,
+  minesNum: 20,
+  isGameOver: false
+}
+
+AppState.board = createEmptyBoard()
 
 function createEmptyBoard () {
   let board = []
-  for (let i = 0; i < rowIndx; i++) {
+  for (let i = 0; i < AppState.rowIndx; i++) {
     let col = []
-    for (let j = 0; j < colIndx; j++) {
+    for (let j = 0; j < AppState.colIndx; j++) {
       col.push({ isClicked: false, mine: false, flag: false })
     }
     board.push(col)
@@ -20,38 +24,37 @@ function createEmptyBoard () {
   return board
 }
 
-
-function getRandom(min, max) {
+function getRandom (min, max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
 
 function reset () {
-  board = createEmptyBoard ()
-  for (let i = 0; i < minesNum; i++) {
-    board[getRandom(0, rowIndx)][getRandom(0, colIndx)].mine = true
+  AppState.board = createEmptyBoard()
+  for (let i = 0; i < AppState.minesNum; i++) {
+    AppState.board[getRandom(0, AppState.rowIndx)][getRandom(0, AppState.colIndx)].mine = true
   }
-  isGameOver = false
+  AppState.isGameOver = false
   render()
 }
 
 reset()
 
-function App (board) {
+function App (state) {
   let resetButton = <div onClick={reset} className='emoji'>&#9786;</div>
-  if (isGameOver) {
+  if (AppState.isGameOver) {
     resetButton = <div onClick={reset} className='emoji'>&#9785;</div>
   }
   return (
-   <div className='app'>
-     <div className='score'>
-       <div className='score1'>000</div>
-       {resetButton}
-       <div className='score2'>000</div>
-     </div>
-     <div className='board'>
-      {Row(board)}
-     </div>
-   </div>
+    <div className='app'>
+      <div className='score'>
+        <div className='score1'>000</div>
+        {resetButton}
+        <div className='score2'>000</div>
+      </div>
+      <div className='board'>
+        {Row(state.board)}
+      </div>
+    </div>
   )
 }
 
@@ -64,19 +67,19 @@ function checkAround (xStr, yStr) {
   const y2 = y + 1
   let values = []
 
-  if (x0 >= 0 && y0 >= 0) values.push(board[x0][y0].mine)
-  if (x0 >= 0) values.push(board[x0][y].mine)
-  if (x0 >= 0 && y2 < rowIndx) values.push(board[x0][y2].mine)
+  if (x0 >= 0 && y0 >= 0) values.push(AppState.board[x0][y0].mine)
+  if (x0 >= 0) values.push(AppState.board[x0][y].mine)
+  if (x0 >= 0 && y2 < AppState.rowIndx) values.push(AppState.board[x0][y2].mine)
 
-  if (y0 >= 0) values.push(board[x][y0].mine)
-  // let val5 = values.push(board[x][y].mine)
-  if (y2 < rowIndx) values.push(board[x][y2].mine)
+  if (y0 >= 0) values.push(AppState.board[x][y0].mine)
+  // let val5 = values.push(AppState.board[x][y].mine)
+  if (y2 < AppState.rowIndx) values.push(AppState.board[x][y2].mine)
 
-  if (x2 < colIndx && y0 >= 0) values.push(board[x2][y0].mine)
-  if (x2 < colIndx) values.push(board[x2][y].mine)
-  if (x2 < colIndx && y2 < rowIndx) values.push(board[x2][y2].mine)
+  if (x2 < AppState.colIndx && y0 >= 0) values.push(AppState.board[x2][y0].mine)
+  if (x2 < AppState.colIndx) values.push(AppState.board[x2][y].mine)
+  if (x2 < AppState.colIndx && y2 < AppState.rowIndx) values.push(AppState.board[x2][y2].mine)
 
-  const mines = values.filter((item)=> {
+  const mines = values.filter((item) => {
     return (item)
   })
   const minesNum = mines.length
@@ -84,51 +87,54 @@ function checkAround (xStr, yStr) {
 }
 
 function handleClick (e) {
-  if (!isGameOver) {
+  if (!AppState.isGameOver) {
     let x = e.target.dataset.row
     let y = e.target.dataset.col
-    board[x][y].isClicked = true
+    AppState.board[x][y].isClicked = true
   }
   render()
 }
 
 function rightClick (e) {
   e.preventDefault()
-  if (!isGameOver) {
+  if (!AppState.isGameOver) {
     let x = e.target.dataset.row
     let y = e.target.dataset.col
-    board[x][y].flag = !board[x][y].flag
+    AppState.board[x][y].flag = !AppState.board[x][y].flag
   }
   render()
 }
 
 function Squares (squares, rowIndex) {
-  let squaresCollection = squares.map((square, i)=> {
+  let squaresCollection = squares.map((square, i) => {
     let classVal = 'square'
     let minesNumber = null
     if (square.flag) classVal = 'square flag'
     if (square.isClicked) {
-        if (square.mine) {
-          classVal = 'square mine'
-          isGameOver = true
-        }
-        if (!square.mine) {
-          classVal = 'square off'
-          minesNumber = checkAround(rowIndex, i)
-        }
+      if (square.mine) {
+        classVal = 'square mine'
+        AppState.isGameOver = true
+      }
+      if (!square.mine) {
+        classVal = 'square off'
+        minesNumber = checkAround(rowIndex, i)
+      }
     }
-    return <div onContextMenu={rightClick}
-                onClick={handleClick}
-                data-row={rowIndex}
-                data-col={i} key={i}
-                className={classVal}>{minesNumber}
-          </div>
+    return (
+      <div
+        onContextMenu={rightClick}
+        onClick={handleClick}
+        data-row={rowIndex}
+        data-col={i} key={i}
+        className={classVal}>{minesNumber}
+      </div>
+    )
   })
   return squaresCollection
 }
 
 function Row (board) {
-  let rowCollection = board.map((item, i)=> {
+  let rowCollection = board.map((item, i) => {
     return <div key={i} className='row'>{Squares(item, i)}</div>
   })
   return rowCollection
@@ -136,7 +142,7 @@ function Row (board) {
 
 function render () {
   ReactDOM.render(
-    App(board),
+    App(AppState),
     document.getElementById('root')
   )
 }
